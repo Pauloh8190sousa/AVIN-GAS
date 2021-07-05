cecf7dfc695b7d40f572c20f9fb0b3a1c29a664d
Pauloh8190sousa <paulohenrique8190@gmail.com>
2021-07-05 15:39:11 -0300

    Remodelagem do Header e estilização

--- src/components/Card Question/index.js
+++ src/components/Card Question/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import {PrimeReactCSS} from "primereact/resources/themes/fluent-light/theme.css";
 import {PrimeReactMinCsss} from "primereact/resources/primereact.min.css";
 import {Icons} from "primeicons/primeicons.css";
-import { RadioButton } from 'primereact/radiobutton';
 
 function CardQuestion(){
     return(
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,21 +4,33 @@ import Logo from "../../assets/styles/imgs/ifpb.png";
 
 import Avin from "../../assets/styles/imgs/avin.png";
 
+import {PrimeReactCSS} from "primereact/resources/themes/fluent-light/theme.css";
+
+import {PrimeReactMinCsss} from "primereact/resources/primereact.min.css";
+
+import {Icons} from "primeicons/primeicons.css";
+
+import {StylesHeader} from "../Header/styles.css";
+
+import {Button} from "primereact/button";
+
+import {Toolbar} from "primereact/toolbar";
+
 function Header() {
     
     const leftContents = (
         <div>
-            <button id="separador">
-            <i className="pi pi-bars p-toolbar-separator p-mr-2" style={{"fontSize":"3em"}}/>
-            </button>
+            <Button id="separador">
+            <i className="pi pi-bars p-toolbar-separator p-mr-2" style={{"fontSize":"2em"}}/>
+            </Button>
 
-            <button id="ajuda">
-            <i id="ajuda" className="pi pi pi-question-circle" style={{"fontSize":"3em"}}/>
-            </button>
+            <Button id="ajuda">
+            <i id="ajuda" className="pi pi pi-question-circle" style={{"fontSize":"2em"}}/>
+            </Button>
             
-            <button id="sign-out">
-            <i className="pi pi-sign-out" style={{"fontSize":"3em"}}/>
-            </button>
+            <Button id="sign-out">
+            <i className="pi pi-sign-out" style={{"fontSize":"2em"}}/>
+            </Button>
             
             
         </div>
@@ -26,17 +38,17 @@ function Header() {
     
     const rightContents = (
         <div>
-            <p id="usuario">
+            <span id="usuario">
              giuseppe@academico.ifpb.edu.br
-            </p>
-            <img id="logo" src={Logo} width="40" height="50"></img>
-            <img id="avin" src={Avin} width="90" height="40"></img>
+            </span>
+            <img id="logo" src={Logo} width="40" height="40"></img>
+            < img id="avin" src={Avin} width="90" height="40"></img>
         </div>
     );
 
     return (
         <div>
-
+            <Toolbar id = "Toolbar" left={leftContents} right={rightContents}></Toolbar>
         </div>
         );
 }
